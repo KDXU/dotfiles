9e25822df6746f60e22cb05479568b0e567df9e4
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2017-01-24 18:02:41 +0900

    add diff-highlight

--- config.fish
+++ config.fish
@@ -3,3 +3,4 @@ set -x JAVA_HOME (/usr/libexec/java_home)
 set fish_theme agnoster
 set PATH $PATH /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/share/dotnet
 set PATH $PATH $ANDROID_HOME/bin
+set PATH $PATH /usr/local/share/git-core/contrib/diff-highlight
