7ed044ee88b508758dfe77d1c0493560b5e52804
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2017-02-08 17:38:35 +0900

    fix for aws

--- config.fish
+++ config.fish
@@ -1,9 +1,7 @@
-tmux  new-session
-
 set -U ANDROID_HOME /usr/local/Cellar/android-sdk/24.4.1_1
 set -x JAVA_HOME (/usr/libexec/java_home)
 set fish_theme agnoster
-set PATH $PATH /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/share/dotnet
+set PATH $PATH ~/Library/Python/2.7/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/share/dotnet
 set PATH $PATH $ANDROID_HOME/bin
 set PATH $PATH /usr/local/share/git-core/contrib/diff-highlight
 set PATH $PATH /usr/local/opt/go/libexec/bin
