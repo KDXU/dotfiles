c69faeae5d116397c715ff5202035b8ccd753501
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2017-11-05 21:27:31 +0900

    update denite configs

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,18 @@
+if status --is-login
+    set -x PATH $PATH /usr/local/bin
+    set -x PATH $PATH ~/bin
+    set -x PATH $PATH ~/.cargo/bin
+    set -x PATH $PATH /usr/local/share/git-core/contrib/diff-highlight
+    set -x PATH $PATH ~/packages/otp/20.0.4/bin
+    set -x PATH $PATH ~/packages/elixir/1.5.2/bin
+    set -x SSH_AUTH_SOCK ~/.gnupg/S.gpg-agent.ssh
+end
+if status --is-login
+    set -x PATH $PATH /usr/local/bin
+    set -x PATH $PATH ~/bin
+    set -x PATH $PATH ~/.cargo/bin
+    set -x PATH $PATH /usr/local/share/git-core/contrib/diff-highlight
+end
 
-
-
-
-
+alias vim "nvim"
+alias tis "tig status"
--- config/fish/env.fish
+++
@@ -1,4 +0,0 @@
-set -x PATH "/usr/local/bin"
-set -x PATH "$HOME/.cargo/bin"
-set -x PATH "$HOME/packages/${ELIXIR_VERSION}/bin"
-set -x PATH "/usr/local/share/git-core/contrib/diff-highlight"
