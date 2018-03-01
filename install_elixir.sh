973d4dcc61bee06cf4623f9680b555e0da5813d3
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2018-03-01 10:33:56 +0900

    bump elixir version

--- install_elixir.sh
+++ install_elixir.sh
@@ -4,7 +4,7 @@
 echo "Install Elixir-${ELIXIR_VERSION}"
 
 curl -fSL -o elixir-precompiled.zip https://github.com/elixir-lang/elixir/releases/download/${ELIXIR_VERSION}/Precompiled.zip
-echo "${ELIXIR_DOWNLOAD_SHA256} elixir-precompiled.zip" | sha256sum -c -
+echo $(shasum -a 256 elixir-precompiled.zip)
 unzip -d ~/packages/elixir/${ELIXIR_VERSION}/ elixir-precompiled.zip
 rm elixir-precompiled.zip
 echo "Completed"
